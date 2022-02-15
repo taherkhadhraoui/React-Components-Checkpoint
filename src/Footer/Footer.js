49db275454351a9ee05600e9ff4ed2769bcb99bd
taherkhadhraoui <taherbenhedi@gmail.com>
2022-02-15 17:26:09 +0100

    my first commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const FooterC = () => {
+  return (
+    <div>
+      <footer className="foter">
+        <p>
+          Don’t miss this chance ! <button>REGISTER NOW</button>{" "}
+        </p>
+        <p>Copyright 2022 - @Taher KHADHRAOUI</p>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterC;
